b8d135d85b3f455e3f5f0019406de87485b18e83
zhouhaoxuan <1508560741@qq.com>
2016-04-20 12:02:02 +0800

    new

--- GitDemo/ViewController.m
+++ GitDemo/ViewController.m
@@ -17,6 +17,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    int a = 5;
+    
+    int b = 6;
+    
+    NSLog(@"%d",a+b);
+    
 }
 
 - (void)didReceiveMemoryWarning {
